3da3d99752fa65db40c9d616bd9628ec6a2ef844
xcoderkhan <khan.m.hasanuzzaman@gmail.com>
2023-08-17 13:59:48 +0600

    Banner Text added.

--- src/layout/pages/Home/Banner/Banner.jsx
+++ src/layout/pages/Home/Banner/Banner.jsx
@@ -21,16 +21,20 @@ const Banner = () => {
             <div className="carousel w-full mt-5 mb-5">
                 {imageUrls.map((imageUrl, index) => (
                     <div key={`slide${index}`} className={`carousel-item relative w-full h-[600px] ${currentSlide === index ? 'visible' : 'hidden'}`}>
-                        <img src={imageUrl} alt={`Slide ${index}`} className="w-full" />
-                        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                        <img src={imageUrl} alt={`Slide ${index}`} className="w-full rounded-xl" />
+
+                        <div className="absolute flex transform -translate-y-1/2 left-5 right-5 bottom-0">
                             <div className=''>
-                                <h2>Affordable Price For Car Servicing</h2>
-                                <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
-                                <div className='flex sm:flex-row'>
+                                <h2 className=' text-6xl text-white font-semibold w-[450px] h-[225px]'>Affordable Price For Car Servicing</h2>
+                                <p className=' text-white w-[525px] h-14 '>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                                <div>
                                     <button className="btn bg-[#FF3811] hover:bg-cyan-400 text-white w-44 h-14 border-none mr-4">Button</button>
                                     <button className="btn btn-outline text-white w-44 h-14">Default</button>
                                 </div>
                             </div>
+                        </div>
+
+                        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
                             <button onClick={() => goToSlide((index + imageUrls.length - 1) % imageUrls.length)} className="btn btn-circle mr-3">❮</button>
                             <button onClick={() => goToSlide((index + 1) % imageUrls.length)} className="btn btn-circle bg-[#FF3811] border-none">❯</button>
                         </div>
