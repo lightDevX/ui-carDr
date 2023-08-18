bc9c0b4654584deba6098108aadbecd088b8b613
MD HASANUZZAMAN KHAN <khan.m.hasanuzzaman@gmail.com>
2023-08-18 08:42:59 +0600

    banner text positioning fixed

--- src/layout/pages/Home/Banner/Banner.jsx
+++ src/layout/pages/Home/Banner/Banner.jsx
@@ -23,11 +23,11 @@ const Banner = () => {
                     <div key={`slide${index}`} className={`carousel-item relative w-full h-[600px] ${currentSlide === index ? 'visible' : 'hidden'}`}>
                         <img src={imageUrl} alt={`Slide ${index}`} className="w-full rounded-xl" />
 
-                        <div className="absolute flex transform -translate-y-1/2 left-5 right-5 bottom-0">
-                            <div className=''>
+                        <div className="absolute flex h-full bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+                            <div className=' space-y-4 pl-24 pt-24'>
                                 <h2 className=' text-6xl text-white font-semibold w-[450px] h-[225px]'>Affordable Price For Car Servicing</h2>
                                 <p className=' text-white w-[525px] h-14 '>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
-                                <div>
+                                <div className=' lg:flex sm:grid sm:grid-flow-col'>
                                     <button className="btn bg-[#FF3811] hover:bg-cyan-400 text-white w-44 h-14 border-none mr-4">Button</button>
                                     <button className="btn btn-outline text-white w-44 h-14">Default</button>
                                 </div>
