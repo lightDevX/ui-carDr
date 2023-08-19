9c647fc9f3b2883022f2052efffca1c737c5c7ad
xcoderkhan <khan.m.hasanuzzaman@gmail.com>
2023-08-19 14:11:31 +0600

    firebase component create

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/Router/Router.jsx'
+import AuthProvider from './routes/Auth/AuthProvider/AuthProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div className='mx-auto max-w-7xl'>
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </React.StrictMode>
   </div>
 )
