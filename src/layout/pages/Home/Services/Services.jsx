721b0232f2aa86f1cfedceeaf3dd27b7edbb6b81
xcoderkhan <khan.m.hasanuzzaman@gmail.com>
2023-08-28 13:03:13 +0600

    Code Update

--- src/layout/pages/Home/Services/Services.jsx
+++ src/layout/pages/Home/Services/Services.jsx
@@ -18,9 +18,9 @@ const Services = () => {
                 <h5 className="text-5xl font-bold">
                     Our Service Area
                 </h5>
-                <p>the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. </p>
+                <p className=" mx-auto mt-4 w-[700px] h-20">the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. </p>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-3">
                 {
                     services.map(service => <ServicesCard
                         key={service._id}
