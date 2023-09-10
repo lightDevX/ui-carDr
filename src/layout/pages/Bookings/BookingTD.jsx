247bb98e6ace89a59aa5201f568782ca55163b89
MD HASANUZZAMAN KHAN <khan.m.hasanuzzaman@gmail.com>
2023-09-10 15:38:43 +0600

    swal implement into the booking part

--- src/layout/pages/Bookings/BookingTD.jsx
+++ src/layout/pages/Bookings/BookingTD.jsx
@@ -1,15 +1,14 @@
 
+const BookingTD = ({ booking, handleDelete }) => {
 
-const BookingTD = ({ booking }) => {
-
-    const { img, customerName, date, price } = booking;
+    const { _id, img, service, date, price } = booking;
 
     return (
         <tr>
             <th>
-                <label>
-                    <input type="checkbox" className="checkbox" />
-                </label>
+                <button onClick={() => handleDelete(_id)} className="btn btn-circle btn-xs">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                </button>
             </th>
             <td>
                 <div className="flex items-center space-x-3">
@@ -19,7 +18,7 @@ const BookingTD = ({ booking }) => {
                         </div>
                     </div>
                     <div>
-                        <div className="font-bold">{customerName}</div>
+                        <div className="font-bold">{service}</div>
                     </div>
                 </div>
             </td>
@@ -30,7 +29,7 @@ const BookingTD = ({ booking }) => {
                 <p>{date}</p>
             </td>
             <th>
-                <button className="btn btn-ghost btn-xs">details</button>
+                <button className="btn btn-ghost btn-xs rounded bg-[#FF3811] text-white">details</button>
             </th>
         </tr>
     );
--- src/layout/pages/Bookings/Bookings.jsx
+++ src/layout/pages/Bookings/Bookings.jsx
@@ -2,20 +2,55 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../../routes/Auth/AuthProvider/AuthProvider";
 import BookingTD from "./BookingTD";
-
+import Swal from "sweetalert2";
 
 const Bookings = () => {
-
     const { user } = useContext(AuthContext);
     const [bookings, setBookings] = useState([]);
-
     const url = `http://localhost:5000/checkout?email=${user?.email}`;
 
     useEffect(() => {
         fetch(url)
             .then(res => res.json())
-            .then(data => setBookings(data))
-    }, [])
+            .then(data => setBookings(data));
+    }, []);
+
+    const handleDelete = (id) => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/checkout/${id}`, {
+                    method: "DELETE"
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            const remaining = bookings.filter(booking => booking._id !== id);
+                            setBookings(remaining);
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            );
+                        } else {
+                            Swal.fire(
+                                'Error!',
+                                'Failed to delete the booking.',
+                                'error'
+                            );
+                        }
+                    });
+            }
+        });
+    };
 
     return (
         <div className="overflow-x-auto">
@@ -35,13 +70,13 @@ const Bookings = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {
-                        bookings.map(booking => <BookingTD
+                    {bookings.map(booking => (
+                        <BookingTD
                             key={booking._id}
                             booking={booking}
-                        ></BookingTD>)
-                    }
-
+                            handleDelete={handleDelete} // Pass the booking ID to handleDelete
+                        />
+                    ))}
                 </tbody>
             </table>
         </div>
